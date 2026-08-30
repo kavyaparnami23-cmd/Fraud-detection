40a25aaae7e7b15b263bea7d9f0505ad15372652
Kavya Parnami <kavya.parnami23@st.niituniversity.in>
2026-08-30 18:23:34 +0530

    Dockerization done

--- backend/index.js
+++ backend/index.js
@@ -14,7 +14,10 @@ const app = express();
 connectDB();
 
 /* Middleware */
-app.use(cors());
+app.use(cors({
+  origin: process.env.CORS_ORIGIN || "http://localhost:5173",
+  credentials: true
+}));
 app.use(express.json());
 app.use(passport.initialize());
 
